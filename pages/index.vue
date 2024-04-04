f1a957ef32afe7f9ccdadc95d33b2f2f2e73b708
Marco Manzo <marcomanzoruiz@gmail.com>
2024-04-04 16:30:48 -0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p class="text-h1">Hola</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
